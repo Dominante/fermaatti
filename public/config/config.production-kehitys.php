2352646a75bc8db8859ba2711ed2fcf1b8b2da6b
Atte Keinänen <atte.keinanen@gmail.com>
2015-09-26 17:03:04 +0300

    Add production kehitys environment config

---
+++ public/config/config.production-kehitys.php
@@ -0,0 +1,28 @@
+<?php
+/* Ympäristömuuttujat tulevat Apachen configista
+ * (/etc/apache2/sites-available/...)
+ */
+define('DEBUG',true);
+$CONFIG = array (
+  'instanceid' => 'ock4xi9fsk9t',
+  'dbtype' => 'mysql',
+  'dbhost' => 'localhost',
+  'dbname' => getenv('DBNAME'),
+  'dbuser' => getenv('DBUSER'),
+  'dbpassword' => getenv('DBPASSWORD'),
+  'passwordsalt' => '6blnPnnjeEBdwKhckX+nE+Lwr+onf3',
+  'secret' => 'WGCzfQX0xPRTo0gfwqiqmFw44cPFU1iHisukMaYluJHTu3SD',
+  'trusted_domains' => array(
+    0 => 'fermaatti.dominante.fi'
+  ),
+  'datadirectory' => '/var/wwwhome/jasensivut-kehitys/data',
+  'overwrite.cli.url' => 'http://fermaatti.dominante.fi/jasensivut-kehitys',
+  'version' => '8.1.0.8',
+  'dbtableprefix' => 'oc_',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  //'memcache.local' => '\\OC\\Memcache\\Redis',
+  'loglevel' => 0,
+  'updatechecker' => false,    // Päivitysmuistutukset pois
+  'default_language' => 'fi_FI'
+);
